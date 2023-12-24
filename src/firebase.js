9ca880fea59422f35dd2700d54ccb6a6854194d4
Priyanshu Mishra <115878284+Priyanshu471@users.noreply.github.com>
2023-12-24 14:47:06 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCAnQBQvB8XSv2w98f8chf2GLQEY_S7p1E",
+//   authDomain: "convozon.firebaseapp.com",
+//   projectId: "convozon",
+//   storageBucket: "convozon.appspot.com",
+//   messagingSenderId: "800625269972",
+//   appId: "1:800625269972:web:494553c6c12de147b8af41"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCAnQBQvB8XSv2w98f8chf2GLQEY_S7p1E",
-  authDomain: "convozon.firebaseapp.com",
-  projectId: "convozon",
-  storageBucket: "convozon.appspot.com",
-  messagingSenderId: "800625269972",
-  appId: "1:800625269972:web:494553c6c12de147b8af41"
+  apiKey: "AIzaSyB5Kkw8b8sA8RZHPe3fsbOVZRGxN3FNiKs",
+  authDomain: "chatapp-c3416.firebaseapp.com",
+  projectId: "chatapp-c3416",
+  storageBucket: "chatapp-c3416.appspot.com",
+  messagingSenderId: "999981076255",
+  appId: "1:999981076255:web:2d452ef8e73b12f5d8ea2a"
 };
 
 // Initialize Firebase
